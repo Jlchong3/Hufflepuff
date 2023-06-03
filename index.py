e6caf504e5efe5f77429cf8374903298bf10956b
Jose Luis <jchongac@fiec.espol.edu.ec>
2023-06-03 15:28:22 -0500

    Agregando tablas

--- index.py
+++ index.py
@@ -7,23 +7,24 @@ from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 import dash_bootstrap_components as dbc
 from pages import equipos, analisis, home, jose, miguel, darwin
+from pages.teams import Atlanta, Boston, Brooklyn, Charlotte, Chicago, Cleveland, Dallas, Denver, Detroit, Golden_State, Houston, Indiana, LAClippers, LALakers, Memphis,Miami, Milwaukee, Minnesota, New_Orleans, New_York,Oklahoma_City, Orlando, Philadelphia, Phoenix, Portland, Sacramento, San_Antonio, Toronto, Utah, Washington
 
 from app import server
 from app import app
 
 dropdown = dbc.DropdownMenu(
     children=[
-        dbc.DropdownMenuItem("Home", href="/home"),
-        dbc.DropdownMenuItem("Equipos", href="/equipos"),
-        dbc.DropdownMenuItem("Analisis", href="/analisis"),
-        dbc.DropdownMenuItem("Darwin", href='/darwin'),
-        dbc.DropdownMenuItem("Miguel", href='/miguel')
-
+        dbc.DropdownMenuItem("Home", href = "/home", id = 'home',),
+        dbc.DropdownMenuItem("Equipos", href = "/equipos", id = 'equipos'),
+        dbc.DropdownMenuItem("Analisis", href = "/analisis", id = 'analisis'),
+        dbc.DropdownMenuItem("Darwin", href = '/darwin', id = 'darwin'),
+        dbc.DropdownMenuItem("Miguel", href = '/miguel', id = 'miguel')
     ],
     nav = True,
     in_navbar = True,
     label = "Secciones",
-    style={'fontSize':'20px'}
+    style={'fontSize':'20px'},
+
 
 )
 
@@ -79,20 +80,114 @@ app.layout = html.Div([
 
 
 @app.callback(Output('page-content', 'children'),
+              Output('equipos', 'active'),
+              Output('analisis', 'active'),
+              Output('darwin', 'active'),
+              Output('miguel', 'active'),
+              Output('home', 'active'),
                [Input('url', 'pathname')])
 def display_page(pathname):
-      if pathname == '/equipos':
-          return equipos.layout
+      if  pathname == '/equipos':
+          return equipos.layout, True, False, False, False, False
       elif pathname == '/analisis':
-          return analisis.layout
+          return analisis.layout, False, True, False, False, False
       elif pathname == '/darwin':
-          return darwin.layout
+          return darwin.layout, False, False, True, False, False
       elif pathname == '/miguel':
-          return miguel.layout
+          return miguel.layout, False, False, False, True, False
       elif pathname == '/jose':
-          return jose.layout
+          return jose.layout, False, False, False, False, False
+      elif pathname[8:] == '/Atlanta':
+        return Atlanta.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Boston':
+        return Boston.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Brooklyn':
+        return Brooklyn.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Charlotte':
+        return Charlotte.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Chicago':
+        return Chicago.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Cleveland':
+        return Cleveland.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Dallas':
+        return Dallas.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Denver':
+        return Denver.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Detroit':
+        return Detroit.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Golden State':
+        return Golden_State.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Houston':
+        return Houston.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Indiana':
+        return Indiana.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/L.A.Clippers':
+        return LAClippers.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/L.A.Lakers':
+        return LALakers.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Memphis':
+        return Memphis.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Miami':
+        return Miami.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Milwaukee':
+        return Milwaukee.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Minnesota':
+        return Minnesota.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/New Orleans':
+        return New_Orleans.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/New York':
+        return New_York.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Oklahoma City':
+        return Oklahoma_City.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Orlando':
+        return Orlando.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Philadelphia':
+        return Philadelphia.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Phoenix':
+        return Phoenix.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Portland':
+        return Portland.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Sacramento':
+        return Sacramento.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/San Antonio':
+        return San_Antonio.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Toronto':
+        return Toronto.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Utah':
+        return Utah.layout, False, False, False, False, False
+    
+      elif pathname[8:] == '/Washington':
+        return Washington.layout, False, False, False, False, False
       else:
-          return home.layout
+          return home.layout, False, False, False, False, True
 
 
 if __name__ == '__main__':
---
+++ pages/teams/LAClippers.py
@@ -0,0 +1,48 @@
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+import dash
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
+import dash_bootstrap_components as dbc
+from dash import dash_table
+
+from app import app        
+    
+df_1 = pd.read_csv('NBA_P.csv')
+
+df_1.drop(columns=['Unnamed: 0.1','Unnamed: 0'])       
+            
+df = df_1[df_1 == 'L.A.Clippers']
+df.drop(columns=['Tm'])
+dropdown = dcc.Dropdown(['2021-2022','2020-2021','2019-2020','2018-2019','2017-2018',
+                         '2016-2017','2015-2016','2014-2015','2013-2014','2012-2013',
+                         '2011-2012','2010-2011','2009-2010','2008-2009','2007-2008',
+                         '2006-2007','2005-2006','2004-2005','2003-2004','2002-2003',
+                         '2001-2002', '2000-2001','1999-2000','1998-1999','1997-1998'],
+                         value = '2021-2022' , label = 'Seleccione Temporada', id = 'L.A.Clippers-players')
+
+layout = dbc.Container([
+    html.Div([
+        html.H1('L.A.Clippers Clippers')
+    ]),
+    html.Div(
+        dbc.Table(id = "Clippers-table"),
+)])
+
+@app.callback(
+    Output('Clippers-table', 'children'),
+    Input('L.A.Clippers-players','value'))
+def filter_year(value):
+    df_year = df[value == 'Season']
+    df_year.drop(columns=['Season'])
+    header = html.Thead(html.Tr([html.Td(i) for i in df_year.columns]))
+    rows = []
+    for i in range(df_year.size):
+        player = []
+        for data in df_year.loc[i]:
+            player.append(html.Td(data))
+        rows.append(html.Tr(player))
+    return dbc.Table(header + html.Tbody(rows), bordered=True)
