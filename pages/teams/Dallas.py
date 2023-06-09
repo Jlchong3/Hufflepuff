436b8bd773f3860117169878a4ccb7f76c16ec5c
Jose Luis <jchongac@fiec.espol.edu.ec>
2023-06-08 22:01:56 -0500

    Arreglado de estilos en las tablas

--- pages/teams/Dallas.py
+++ pages/teams/Dallas.py
@@ -22,7 +22,7 @@ dropdown = dcc.Dropdown(['2021-22','2020-21','2019-20','2018-19','2017-18',
                          '2011-12','2010-11','2009-10','2008-09','2007-08',
                          '2006-07','2005-06','2004-05','2003-04','2002-03',
                          '2001-02', '2000-01','1999-00','1998-99','1997-98'],
-                         value = '2021-2022' , id = 'Dallas-players', style={'marginTop':'1vw','marginBottom':'1vw'})
+                         value = '2021-2022' , id = 'Dallas-players', style={'marginTop':'1vw','marginBottom':'2vw'})
 
 layout = dbc.Container([
     html.Div([
@@ -30,7 +30,7 @@ layout = dbc.Container([
     ]),
     html.Div([dropdown]),
     html.Div(
-        dbc.Table(id = "Mavericks-table"),
+        dbc.Table(id = "Mavericks-table", color = "info", hover = True),
 )])
 
 @app.callback(
@@ -40,7 +40,7 @@ def filter_year(value):
     df_year = df[value == df['Season']]
     if 'Season' in df_year.columns:
         df_year.drop(columns=['Season'], inplace=True)
-    header = [html.Thead(html.Tr([html.Td(i,style={'Fontweight':'Bold'}) for i in df_year.columns]))]
+    header = [html.Thead(html.Tr([html.Td(i) for i in df_year.columns]),style={'fontWeight':'Bold','fontSize':'17px'})]
     rows = []
     for i in range(df_year.shape[0]):
         player = []
